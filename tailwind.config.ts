feb32cea16ade167c7372a8f8de96886ea7ffd0c
Ross Root <rossroot@proton.me>
2025-01-14 15:44:33 -0800

    scaffold skeleton ui sveltekit project

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import containerQueries from '@tailwindcss/container-queries';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+import { contentPath, skeleton } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		skeleton({
+			themes: [themes.legacy]
+		}),
+		typography,
+		forms,
+		containerQueries
+	]
+} satisfies Config;
