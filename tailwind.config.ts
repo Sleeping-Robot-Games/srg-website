8cd9068dce9c6fb4a435dd3af6585aab748b1391
Ross Root <git@rossroot.com>
2026-04-13 17:21:35 -0700

    update packages, address vulnerabilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,20 +3,10 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-import { contentPath, skeleton } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: [
-		skeleton({
-			themes: [themes.legacy]
-		}),
-		typography,
-		forms,
-		containerQueries
-	]
+	plugins: [typography, forms, containerQueries]
 } satisfies Config;
